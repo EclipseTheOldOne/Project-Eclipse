7027ae54ee0051ebe51bdb2a7937b988df5bdf7b
EclipseTheOldOne <linhvu1234512345@GMAIL.COM>
2021-12-13 14:10:57 +0700

    a

---
+++ scripts/blocks/generator/adv-steam.js
@@ -0,0 +1,13 @@
+const steamGen = extend(BurnerGenerator, "adv-steam-gen", {
+    generateFx: Fx.smeltsmoke,
+    getItemEfficiency(liquid) {
+      return item.flammability * (1.5 - liquid.temperature)
+    }
+});
+steamGen.buildType = () => extend(BurnerGenerator.BurnerGeneratorBuild, steamGen, {
+  update(){
+    this.super$update();
+
+    if(this.productionEfficiency > 0.0 && this.generateTime - Time.delta <= 0 && Mathf.chance(0.01)) steamGen.generateFx.at(this.x + Mathf.range(steamGen.generateEffectRnd), this.y + Mathf.range(steamGen.generateEffectRnd));
+  }
+})
---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+require("blocks/generator/adv-steam");
