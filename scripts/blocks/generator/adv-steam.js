1c8079bd2a68743aaecceeefcd955080112ad241
EclipseTheOldOne <linhvu1234512345@GMAIL.COM>
2021-12-14 15:23:44 +0700

    ,m

--- scripts/blocks/generator/adv-steam.js
+++ scripts/blocks/generator/adv-steam.js
@@ -7,7 +7,6 @@ const steamGen = extend(BurnerGenerator, "adv-steam-gen", {
 steamGen.buildType = () => extend(BurnerGenerator.BurnerGeneratorBuild, steamGen, {
   update(){
     this.super$update();
-
     if(this.productionEfficiency > 0.0 && this.generateTime - Time.delta <= 0 && Mathf.chance(0.01)) steamGen.generateFx.at(this.x + Mathf.range(steamGen.generateEffectRnd), this.y + Mathf.range(steamGen.generateEffectRnd));
   }
 })
