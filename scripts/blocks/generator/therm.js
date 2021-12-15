f58ac92f7c30c967021a55719c934beb72c91399
EclipseTheOldOne <linhvu1234512345@GMAIL.COM>
2021-12-15 14:29:30 +0700

    Thermal Boiler!

--- scripts/blocks/generator/therm.js
+++ scripts/blocks/generator/therm.js
@@ -3,14 +3,20 @@ const therM = extend(ThermalGenerator, "thermal-boiler", {
         this.super$load();
         this.region = Core.atlas.find("mindus-thermal-boiler");
         this.regionTop = Core.atlas.find("mindus-thermal-boiler-top");
-        this.regionLiquid = Core.atlas.find("mindus-thermal-boiler-liquid");
+        this.regionLiquid = Core.atlas.find("mindus-thermal-boiler-inLiquid");
     }
 });
-therM.generateEffect = Fx.redgeneratespark
+therM.generateEffect = Fx.redgeneratespark;
+var progressAdd
 therM.buildType = () => extend(ThermalGenerator.ThermalGeneratorBuild, therM, {
     update(){
-        this.super$update()
-        if(this.liquids.total() <= 0.4){this.productionEfficiency = 0}else
-        {if(Mathf.chance(0.085)){therM.generateEffect.at(this.x, this.y)}}
+        this.super$update();
+        if(this.liquids.total() <= 0.4){this.productionEfficiency = 0.35; if(Mathf.chance(0.085)){therM.generateEffect.at(this.x, this.y)}}else
+        {if(Mathf.chance(0.085)){therM.generateEffect.at(this.x, this.y)}};
+    },
+    draw(){
+        this.super$draw();
+        Draw.rect(therM.region, this.x, this.y);
+        Drawf.liquid(therM.regionLiquid, this.x, this.y, this.liquids.total() / therM.liquidCapacity, this.liquids.current().color)
     }
 })
