dc5306e67fecf0663365b8488f23ee9643718a48
EclipseTheOldOne <43229798+EclipseTheOldOne@users.noreply.github.com>
2021-12-16 10:25:31 +0700

    a

--- scripts/blocks/turret/cross-bow.js
+++ scripts/blocks/turret/cross-bow.js
@@ -1,11 +1,7 @@
-const basicCrossBow = extend(ItemTurret, "crossbow", {
-    health: 430,
-    size: 2,
-    targetAir: true,
-    targetGround: true,
-    inaccuracy: 3.5,
-    reloadTime: 40,
-    rotateSpeed: 9,
-    range: 220,
-    description: "A long, powerful tower, shoot with decent speed (wip)\n[accent]Shoot an powerful bolt every 3rd shoot, penetrate though all enermy.",
+const basicCrossBow = extend(ItemTurret, "crossbow", {})
+basicCrossBow.buildType = () => extend(ItemTurret.ItemTurretBuild, basicCrossBow, {
+    shoot(type){
+        this.super$shoot(type)
+        if(a >= 3){print("yo mama")}else{a++}
+    }
 })
---
+++ scripts/libs/bulletLib.js
@@ -0,0 +1,6 @@
+const smallArrowCopper = extend(BasicBulletType, 4.5, 25, "mindus-small-arrow-copper", {
+    height: 16,
+    width: 8,
+    lifetime: 48.89,
+    shrinkY: 0
+})
