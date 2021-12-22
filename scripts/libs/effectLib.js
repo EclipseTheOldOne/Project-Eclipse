cb27752832d6d63ffa1e1a95447e1d68fc809f4f
EclipseTheOldOne <linhvu1234512345@GMAIL.COM>
2021-12-22 11:39:45 +0700

    updated transfering effect

--- scripts/libs/effectLib.js
+++ scripts/libs/effectLib.js
@@ -12,7 +12,21 @@ const mendWave = new Effect(20, e => {
     Draw.alpha(e.fout())
     Fill.circle(e.x, e.y, e.fin() * 180);
 });
+//basically itemtransfer effect
+const itemSlowTransfer = new Effect(120, e => {
+    if(!(e.data instanceof Position)) return;
+    Tmp.v1.set(e.x, e.y).interpolate(Tmp.v2.set(e.data), e.fin(), Interp.pow3).add(Tmp.v2.sub(e.x, e.y).nor().rotate90(1).scl(Mathf.randomSeedRange(e.id, 1) * e.fslope() * 10));
+    var x = Tmp.v1.x, y = Tmp.v1.y;
+    var size = 1;
+
+    Lines.stroke(e.fslope() * 2 * size, Pal.accent);
+    Lines.circle(x, y, e.fslope() * 2 * size);
+
+    Draw.color(e.color);
+    Fill.circle(x, y, e.fslope() * 1.5 * size);
+})
 module.exports = {
     pulseWave:pulseWave,
-    mendWave:mendWave
+    mendWave:mendWave,
+    itemSlowTransfer:itemSlowTransfer
 };
